f3a21e4cf2b3352e8dc129bf821fa3dc043d9397
LEEYANGYANG <liyangyang0713@foxmail.com>
2022-09-23 01:27:15 +0800

    null

---
+++ mybatis07-02/src/xyz/leeyangy/pojo/User.java
@@ -0,0 +1,62 @@
+package xyz.leeyangy.pojo;
+
+import java.util.Date;
+
+public class User {
+    private String id;
+    private String name;
+    private String password;
+    private Integer age;
+    private Date birthday;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", password='" + password + '\'' +
+                ", age=" + age +
+                ", birthday=" + birthday +
+                '}';
+    }
+}
