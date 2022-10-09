0ed1d037a537a20fb28a349bd97848562294446b
LEEYANGYANG <liyangyang0713@foxmail.com>
2022-10-10 00:40:13 +0800

    定义抽象方法
    Signed-off-by: LEEYANGYANG <liyangyang0713@foxmail.com>

--- spring-mybatis/src/main/java/xyz/leeyangy/dao/AccountDao.java
+++ spring-mybatis/src/main/java/xyz/leeyangy/dao/AccountDao.java
@@ -1,6 +1,11 @@
 package xyz.leeyangy.dao.impl;
 
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
 import xyz.leeyangy.domain.Account;
+
+import java.util.List;
 
 /**
  * @Package: xyz.leeyangy.dao.impl
@@ -10,5 +15,19 @@ import xyz.leeyangy.domain.Account;
  */
 public interface AccountDao {
 //    通过id查找用户
+    @Select("SELECT * from t_account WHERE id=#{ids}")
     Account findUserById(Integer ids);
+
+//    查找所有用户
+    @Select("select * from t_account")
+    List<Account> findAll();
+
+//    删除用户
+    @Delete("delete from t_account where id=#{ids}")
+    int delateUserById(Integer ids);
+
+//    新增用户
+    @Insert("")
+    int addUser(Account account);
+
 }
