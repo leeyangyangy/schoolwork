205938e66716257c24ce711a1b7facab8ecacab9
LEEYANGYANG <liyangyang0713@foxmail.com>
2022-10-09 18:40:15 +0800

    数据源管理-文件加载规范
    Signed-off-by: LEEYANGYANG <liyangyang0713@foxmail.com>

--- spring-datasource/src/main/java/xyz/leeyangy/App.java
+++ spring-datasource/src/main/java/xyz/leeyangy/App.java
@@ -2,6 +2,7 @@ package xyz.leeyangy;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import xyz.leeyangy.dao.BookDao;
 
 import javax.sql.DataSource;
 
@@ -10,10 +11,15 @@ import javax.sql.DataSource;
  */
 public class App {
     public static void main(String[] args) {
-        ApplicationContext context=new ClassPathXmlApplicationContext("applicationContext.xml");
-        DataSource dataSource1=(DataSource) context.getBean("dataSourceForDruid");
-        DataSource dataSource2=(DataSource) context.getBean("dataSourceForC3p0");
-        System.out.println(dataSource1);
-        System.out.println(dataSource2);
+//        ApplicationContext context=new ClassPathXmlApplicationContext("applicationContext.xml");
+//        DataSource dataSource1=(DataSource) context.getBean("dataSourceForDruid");
+//        DataSource dataSource2=(DataSource) context.getBean("dataSourceForC3p0");
+//        System.out.println(dataSource1);
+//        System.out.println(dataSource2);
+        ClassPathXmlApplicationContext context=new ClassPathXmlApplicationContext("applicationContext.xml");
+//        DataSource dataSource=(DataSource) context.getBean("dataSource");
+//        System.out.println(dataSource);
+        BookDao bookDao=(BookDao) context.getBean("bookDao");
+        bookDao.save();
     }
 }
