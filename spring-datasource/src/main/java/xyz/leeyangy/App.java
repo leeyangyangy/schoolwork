38f7a1e84e78a6a0bbc30f1cc9d9ccf738f4a72f
LEEYANGYANG <liyangyang0713@foxmail.com>
2022-10-09 18:05:39 +0800

    数据源管理
    Signed-off-by: LEEYANGYANG <liyangyang0713@foxmail.com>

---
+++ spring-datasource/src/main/java/xyz/leeyangy/App.java
@@ -0,0 +1,19 @@
+package xyz.leeyangy;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import javax.sql.DataSource;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext context=new ClassPathXmlApplicationContext("applicationContext.xml");
+        DataSource dataSource1=(DataSource) context.getBean("dataSourceForDruid");
+        DataSource dataSource2=(DataSource) context.getBean("dataSourceForC3p0");
+        System.out.println(dataSource1);
+        System.out.println(dataSource2);
+    }
+}
