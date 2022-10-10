12749ef1905ad41f0b5e990d30ab2b66fc2ede50
LEEYANGYANG <liyangyang0713@foxmail.com>
2022-10-10 09:53:08 +0800

    整合junit
    Signed-off-by: LEEYANGYANG <liyangyang0713@foxmail.com>

--- spring-mybatis/src/test/java/xyz/leeyangy/FunctionTest.java
+++ spring-mybatis/src/test/java/xyz/leeyangy/FunctionTest.java
@@ -1,8 +1,12 @@
 package xyz.leeyangy;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import xyz.leeyangy.config.SpringConfig;
 import xyz.leeyangy.domain.Account;
 import xyz.leeyangy.service.AccountService;
@@ -12,14 +16,24 @@ import java.util.List;
 /**
  * Unit test for simple App.
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = SpringConfig.class)
 public class FunctionTest {
-    @Test
-    public void testSqlSessionFactoryBean(){
+
+    @Autowired
+    private AccountService accountService;
+
+    public static void main(String[] args) {
 //        获取ioc容器
         ApplicationContext ctx=new AnnotationConfigApplicationContext(SpringConfig.class);
         AccountService accountService = ctx.getBean(AccountService.class);
         Account account = accountService.findById(1);
         System.out.println(account);
+    }
+
+    @Test
+    public void testFindById(){
+        System.out.println(accountService.findById(2));
     }
 
 }
