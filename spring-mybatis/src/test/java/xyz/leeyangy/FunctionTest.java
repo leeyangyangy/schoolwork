99e685ceccb6cd6a8a58ebbe8dff6cf59c84ac19
LEEYANGYANG <liyangyang0713@foxmail.com>
2022-10-10 09:26:56 +0800

    删除不需要的代码
    Signed-off-by: LEEYANGYANG <liyangyang0713@foxmail.com>

--- spring-mybatis/src/test/java/xyz/leeyangy/FunctionTest.java
+++ spring-mybatis/src/test/java/xyz/leeyangy/FunctionTest.java
@@ -1,14 +1,11 @@
 package xyz.leeyangy;
 
-import org.apache.ibatis.session.SqlSession;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import xyz.leeyangy.config.SpringConfig;
-import xyz.leeyangy.dao.AccountDao;
 import xyz.leeyangy.domain.Account;
 import xyz.leeyangy.service.AccountService;
-import xyz.leeyangy.utils.MybatisUtils;
 
 import java.util.List;
 
@@ -16,32 +13,6 @@ import java.util.List;
  * Unit test for simple App.
  */
 public class FunctionTest {
-//    @Test
-//    public void testFindAll() {
-////        1.创建SqlSession对象
-//        SqlSession session = null;
-//        try {
-////           获取session
-//            session = MybatisUtils.getSqlSession();
-////            获取接口对象
-//            AccountDao accountDao = session.getMapper(AccountDao.class);
-////            执行功能
-//            List<Account> list = accountDao.findAll();
-////            查询结果
-//            for (Account account:list){
-//                System.out.println(list);
-//            }
-////            提交事务
-//            session.commit();
-//        } catch (Exception e) {
-////                事务回滚
-//            session.rollback();
-//            e.printStackTrace();
-//        } finally {
-////                关闭session
-//            MybatisUtils.close(session);
-//        }
-//    }
     @Test
     public void testSqlSessionFactoryBean(){
 //        获取ioc容器
